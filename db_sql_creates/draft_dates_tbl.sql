7ad6c203c79cf0ec3bc9c09a4ec72d6b6855dce3
notoriousTRON <dtimson2@gmail.com>
2021-09-16 10:22:47 -0500

    moved the draft dates stage into the draft order query. It should all be one process

--- db_sql_creates/draft_dates_tbl.sql
+++
@@ -1,7 +0,0 @@
---DROP TABLE draft_dates_tbl
---CREATE TABLE draft_dates_tbl (draft_year varchar, draft_type varchar, draft_date date)
---SELECT * FROM draft_dates_tbl
-
-INSERT INTO draft_dates_tbl (draft_year, draft_type, draft_date)
---VALUES ('2019','startup','08/26/19')
-VALUES ('2020','rookie','05/02/20')
--- db_sql_creates/draft_order_tbl.sql
+++ db_sql_creates/draft_order_tbl.sql
@@ -1,41 +1,43 @@
-DROP TABLE draft_order_tbl
-CREATE TABLE draft_order_tbl (draft_year varchar, draft_type varchar, draft_order_type varchar, pick_no varchar,roster_id varchar)
-SELECT * FROM draft_order_tbl
 
-INSERT INTO draft_order_tbl (draft_year, draft_type, draft_order_type, pick_no,roster_id)
---VALUES ('2020','rookie','linear','1','6')
---VALUES ('2020','rookie','linear','2','1')
---VALUES ('2020','rookie','linear','3','12')
---VALUES ('2020','rookie','linear','4','3')
---VALUES ('2020','rookie','linear','5','11')
---VALUES ('2020','rookie','linear','6','9')
---VALUES ('2020','rookie','linear','7','4')
---VALUES ('2020','rookie','linear','8','5')
---VALUES ('2020','rookie','linear','9','10')
---VALUES ('2020','rookie','linear','10','7')
---VALUES ('2020','rookie','linear','11','2')
-VALUES ('2020','rookie','linear','12','8')
+--############## START HERE    ##############
+--## INSERT THE DRAFT DATE INTO THE TABLE ##
+--INSERT INTO stg.draft_dates_tbl (draft_year, draft_type, draft_date)
+----VALUES ('2019','startup','08/26/19')
+----VALUES ('2020','rookie','05/02/20')
+--VALUES ('2021','rookie','05/02/21')
 
 
---INSERT INTO draft_order_tbl (draft_year, draft_type, draft_order_type, pick_no,roster_id)
---SELECT d.draft_year, d.draft_type, 'snake' AS draft_order_type ,d.pick_no,d.roster_id FROM draft_tbl d
---LEFT JOIN
---	draft_dates_tbl dd
---ON
---	d.draft_year = dd.draft_year AND
---	d.draft_type = dd.draft_type
---LEFT JOIN
---	(SELECT
---		user_id
---		,roster_id
---		,display_name
---	 	,join_date
---	 	,leave_date
---		,CASE WHEN join_date IS NULL THEN '2019-07-01' ELSE join_date END AS join_date1
---		,CASE WHEN leave_date IS NULL THEN DATE(NOW()) ELSE leave_date END AS leave_date1
---	FROM
---		user_history_tbl) u
---ON
---	d.roster_id = u.roster_id AND
---	dd.draft_date BETWEEN join_date1 AND leave_date1
---WHERE d.round = '2' AND d.draft_year = '2019' ORDER BY CAST(d.pick_no AS INT)
+
+--## USE THE FOLLOWING QUERY TO LOOK UP ROSTER NUMBERS ##
+--SELECT *
+--FROM
+--	stg.user_history_tbl
+
+--## MANUALLY LOOK UP THE ROSTER IDS USING THE QUERY ABOVE AND INSERT EACH ROW INTO THE TABLE ##
+INSERT INTO stg.draft_order_tbl (draft_year, draft_type, draft_order_type, pick_no,roster_id)
+--VALUES ('2021','rookie','linear','1','5')
+--VALUES ('2021','rookie','linear','2','12')
+--VALUES ('2021','rookie','linear','3','7')
+--VALUES ('2021','rookie','linear','4','1')
+--VALUES ('2021','rookie','linear','5','6')
+--VALUES ('2021','rookie','linear','6','11')
+--VALUES ('2021','rookie','linear','7','2')
+--VALUES ('2021','rookie','linear','8','9')
+--VALUES ('2021','rookie','linear','9','4')
+--VALUES ('2021','rookie','linear','10','8')
+--VALUES ('2021','rookie','linear','11','3')
+--VALUES ('2021','rookie','linear','12','10')
+
+
+
+
+--## EVERYTHING BELOW THIS LINE SHOULD ONLY BE USED IF EITHER DRAFT TABLE NEEDS TO BE RECREATED ##
+------------------------------------------------------------------------------------------------------------------------
+
+--DROP TABLE stg.draft_order_tbl
+--CREATE TABLE stg.draft_order_tbl (draft_year varchar, draft_type varchar, draft_order_type varchar, pick_no varchar,roster_id varchar)
+--SELECT * FROM stg.draft_order_tbl
+
+--DROP TABLE stg.draft_dates_tbl
+--CREATE TABLE stg.draft_dates_tbl (draft_year varchar, draft_type varchar, draft_date date)
+--SELECT * FROM stg.draft_dates_tbl
