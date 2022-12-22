02760cbc1a15eb470eed3f98108685b5f5504191
notoriousTRON <dtimson2@gmail.com>
2022-12-22 14:23:02 -0600

    added new aws connection type

--- modules/open_connection.py
+++ modules/open_connection.py
@@ -3,9 +3,15 @@ os.chdir(r'C:\projects\sleeper_data\modules')
 import crds
 import psycopg2 as ps
 
-usr = crds.get_usr()
-pwd = crds.get_pwd()
-
 def open_connection():
-    db = ps.connect(database="dynasty", user="postgres", password="cotton2", host="127.0.0.1",port="5432")
+    usr = crds.get_usr()
+    pwd = crds.get_pwd()
+    db = ps.connect(database="dynasty", user="postgres", password=pwd, host="127.0.0.1",port="5432")
+    return db
+
+def open_connection_cloud():
+    svr = "dynasty1.cchqpfdtasog.us-east-2.rds.amazonaws.com"
+    usr = crds.get_usr_cloud()
+    pwd = crds.get_pwd_cloud()
+    db = ps.connect(database="any_database", user=usr, password=pwd, host=svr,port="5432")
     return db
